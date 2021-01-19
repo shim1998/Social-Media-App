80732af2319efc3a87458384c7e7e299d289143d
helewrer3 <helewrer3@gmail.com>
2021-01-19 23:32:35 +0530

    added comment section

--- src/public/app/all-posts.js
+++ src/public/app/all-posts.js
@@ -5,28 +5,46 @@ async function loadPosts() {
     $.get('/api/posts', {}, (data) => {
         for(let p of data){
             let str = `<a href="#" class="card-link" id="${p.id}">Read More</a>`;
-            getCards(p, 4, str, 200);
+            getCard(p, 4, str, 200, 1);
         }
         $(".card-link").click((ev) => {
             let postId = $(ev.target).attr('id');
             console.log(`/api/posts/${postId}`);
             $.get(`/api/posts/${postId}`, {}, (data) => {
                 $('#posts-container').empty();
-                getCards(data, 12, "", 0);
+                getCard(data, 12, "", 0, 1);
+                createCommentForm();
+                $.get(`/api/comments/${postId}`, {}, (data) => {
+                    for(let p of data)getCard(p, 12, "", 0, 0);
+                })
+                $('#comment-create').click((ev) => {
+                    let body = $('#comment-body').val();
+                    let userdata = JSON.parse(window.localStorage.user);
+                    let id = userdata.id;
+                    $.post('/', {
+                        userId: id,
+                        postId: postId,
+                        body: body
+                    }, (data) => {
+                        console.log('created');
+                    })
+                })
             })
         })
     })
 }
 
-function getCards(data, wdt, str, lim){
-    let body;
+function getCard(data, wdt, str, lim, f){
+    let body, title;
     if(!lim)body = data.body;
     else body = data.body.substr(0, lim) + '...';
+    if(f)title = `<h5 class="card-title">${data.title}</h5>`;
+    else title = "";
     $('#posts-container').append($(`
         <div class="col-${wdt}">
             <div class="card" style="width: auto;">
                 <div class="card-body">
-                    <h5 class="card-title">${data.title}</h5>
+                    ${title}
                     <h6 class="card-subtitle mb-2 text-muted">${data.user.name}</h6>
                     <p class="card-text">${body}</p>
                     ${str}
@@ -35,3 +53,16 @@ function getCards(data, wdt, str, lim){
         </div>
     `))
 }
+
+function createCommentForm(){
+    $('#posts-container').append($(`
+        <div class="col-12">
+            <h1 class="display-6 text-center">Write Comment</h1>
+            <div class="form-group">
+                <textarea class="form-control" id="comment-body"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary" id="comment-create">Create Comment</button>
+        </div>
+    `))
+}
+
--- src/routes/posts/comments.js
+++ src/routes/posts/comments.js
@@ -1,4 +1,16 @@
-const {  } = require('../../controllers/comments');
+const { createComment, getCommentsByPost } = require('../../controllers/comments');
 const route = require('express').Router();
+
+route.post('/', async (req, res) => {
+    let {userId, postId, body} = req.body;
+    if(!userId || !postId || !body)return res.status(404).send('Bad request');
+    let comment = createComment(userId, postId, body);
+    res.status(201).send(comment);
+})
+
+route.get('/:id', async (req, res) => {
+    let comments = getCommentsByPost(req.params.id);
+    res.status(201).send(comments);
+})
 
 module.exports = {commentRoute: route}
